44fff4577edbb309db22992b829381d714b57fc6
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-29 21:18:13 -0300

    initial

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
-  }
-}
-
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
